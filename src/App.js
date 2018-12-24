6e4d8636e8505c87115f95985cc883b790f8b69f
Henk Huitema <hnk.htm@gmail.com>
2018-12-24 11:03:59 +0100

    added production dockerfile

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and do something else together.
+            Edit <code>src/App.js</code> and do so for a while.
           </p>
           <a
             className="App-link"
