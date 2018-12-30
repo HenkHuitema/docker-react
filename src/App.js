0fc9925fe6cb2d06e82392707945890a7086a02c
Henk Huitema <hnk.htm@gmail.com>
2018-12-30 10:34:36 +0100

    changed sorce for testing travis

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and do so for a while.
+            Edit <code>src/App.js</code> and nothing elsed to fo.
           </p>
           <a
             className="App-link"
